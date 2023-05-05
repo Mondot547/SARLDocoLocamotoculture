468eb671272a8846a05c5db93a0eaff21e577029
Replit user <>
2023-05-05 12:55:47 +0000

    verifier le code du formulaire de contact server/client

--- src/back-end/main.js
+++ src/back-end/main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+require('dotenv').config(); // charger les variables d'environnement depuis .env
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,18 +9,37 @@ app.use(bodyParser.json());
 app.post('/api/contact', async (req, res) => {
   const { name, email, message } = req.body;
 
+  // configuration du transporter de nodemailer
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     secure: true,
     auth: {
-      user: 'alexandregerard119@gmail.com',
-      pass: '12Alexsand26?',
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
     },
   });
 
   const mailOptions = {
     from: email,
-    to: 
-  }
-})
+    to: process.env.EMAIL_ENTREPRISE,
+    subject: `Nouveau message de ${name}`,
+    text: message,
+  };
+
+  // envoi de l'email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send('error');
+    } else {
+      console.log('Email envoyé: ' + info.response);
+      res.send('success');
+    }
+  });
+});
+
+const port = process.env.PORT || 3000; // utiliser le port défini dans les variables d'environnement ou le port 3000 par défaut
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,6 @@ import Nav from '../components/menu/Nav';
 import './scss/Contact.scss';
 
 const Contact = () => {
-
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
@@ -15,47 +14,50 @@ const Contact = () => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ name, email, message}),
+      body: JSON.stringify({ name, email, message }),
     });
-    const data = await response.json();
+    const data = await response.text();
     console.log(data);
   };
-  
-    return (
-        <>
-            <header>
-              <Nav />
-            </header>
-            <main className="contact">
-              <form onSubmit={handleSubmit}>
-                <div>
-                  <label htmlFor='name'>Nom</label>
-                  <input
-                    type='text'
-                    id='name'
-                    value={name}
-                    onChange={(e) => setName(e.target.value)} />
-                </div>
-                <div>
-                  <label htmlFor='email'>E-mail</label>
-                  <input
-                    type='email'
-                    id='email'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)} />
-                </div>
-                <div>
-                  <label htmlFor='message'>Message</label>
-                  <textarea
-                    id='message'
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)} />
-                </div>
-                <button type='submit'>Envoyer</button>
-              </form>
-            </main>
-        </>
-    );
+
+  return (
+    <>
+      <header>
+        <Nav />
+      </header>
+      <main className="contact">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='name'>Nom</label>
+            <input
+              type='text'
+              id='name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor='email'>E-mail</label>
+            <input
+              type='email'
+              id='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor='message'>Message</label>
+            <textarea
+              id='message'
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </div>
+          <button type='submit'>Envoyer</button>
+        </form>
+      </main>
+    </>
+  );
 };
 
 export default Contact;
