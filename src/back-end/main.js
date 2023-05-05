7253245e6a00a1157f2b6a9f3d2902ba5f684922
Replit user <>
2023-05-05 11:14:59 +0000

    commencement du cote serveur pour le formulaire de contact

---
+++ src/back-end/main.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/api/contact', async (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'alexandregerard119@gmail.com',
+      pass: '12Alexsand26?',
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: 
+  }
+})
